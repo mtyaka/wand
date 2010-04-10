0bae7de81742d3622968f753e4ab7cd599047b00
Matjaz Gregoric <mtyaka@gmail.com>
2010-04-10 19:46:37 +0200

    Don't run the "file" command if it is not present.
    This makes Wand not error out on systems where the "file" command
    is not present (e.g. Windows).

--- lib/wand.rb
+++ lib/wand.rb
@@ -5,19 +5,20 @@ module Wand
   
   def self.wave(path)
     type = MIME::Types.type_for(path)[0].to_s
-    type = execute_file_cmd(path).split(';')[0].strip if type.nil? || type == ''
-    type = nil if type =~ /cannot\sopen/
+    type = execute_file_cmd(path).split(';')[0].strip if (type.nil? || type == '') && executable
+    type = nil if type =~ /cannot\sopen/ || type == ''
     type
   end
 
   def self.executable
-    @executable ||= `which file`.chomp
+    return @executable if defined?(@executable)
+    @executable = system('which file') ? `which file`.chomp : nil
   end
 
   def self.executable=(path)
     @executable = path
   end
-  
+
   def self.execute_file_cmd(path)
     `#{executable} --mime --brief #{path}`
   end
--- test/test_wand.rb
+++ test/test_wand.rb
@@ -1,11 +1,7 @@
 require 'helper'
 
-class TestWand < Test::Unit::TestCase
-  context "Wand" do
-    setup do
-      Wand.executable = `which file`.chomp
-    end
-
+module WandTestHelpers
+  def mime_types_gem_types
     {
       'AVGARDD.svg'    => 'image/svg+xml',
       'compressed.zip' => 'application/zip',
@@ -14,40 +10,82 @@ class TestWand < Test::Unit::TestCase
       'jquery.js'      => 'application/javascript',
       'ol_tiny.jpg'    => 'image/jpeg',
       'ol_tiny.png'    => 'image/png',
-      'styles.css'     => 'text/css',
-    }.each_pair do |name, type|
-      should "use mime type gem if it returns type #{name}" do
-        assert_equal type, Wand.wave(FilePath.join(name).expand_path.to_s)
-      end
-    end
+      'styles.css'     => 'text/css'
+    }
+  end
 
+  def unix_file_command_types
     {
       'example.m4r'    => 'audio/mp4',
       'AVGARDD.eot'    => 'application/octet-stream',
       'AVGARDD.ttf'    => 'application/octet-stream',
-      'AVGARDD.woff'   => 'application/octet-stream',
-    }.each_pair do |name, type|
-      should "fall back to unix file command when mime type returns nothing for #{name}" do
-        assert_equal type, Wand.wave(FilePath.join(name).expand_path.to_s)
+      'AVGARDD.woff'   => 'application/octet-stream'
+    }
+  end
+end
+
+class TestWand < Test::Unit::TestCase
+  extend WandTestHelpers
+
+  FILE_EXECUTABLE = Wand.executable
+
+  context "Wand" do
+    if !FILE_EXECUTABLE
+      warn 'The "file" command was not found. Skipping tests that assume "file" command is present.'
+    else
+      context "when the file executable exists" do
+        setup do
+          Wand.executable = FILE_EXECUTABLE
+        end
+
+        should "detect executable" do
+          assert_equal '/usr/bin/file', Wand.executable
+        end
+
+        mime_types_gem_types.each_pair do |name, type|
+          should "use mime type gem if it returns type #{name}" do
+            assert_equal type, Wand.wave(FilePath.join(name).expand_path.to_s)
+          end
+        end
+
+        unix_file_command_types.each_pair do |name, type|
+          should "fall back to unix file command when mime type returns nothing for #{name}" do
+            assert_equal type, Wand.wave(FilePath.join(name).expand_path.to_s)
+          end
+        end
+
+        should "return nil when mime type and file fail" do
+          assert_nil Wand.wave('AVGARDD.eot')
+        end
+
+        should "allow setting the executable" do
+          Wand.executable = '/usr/local/bin/file'
+          assert_equal '/usr/local/bin/file', Wand.executable
+        end
+
+        should "strip newlines and such" do
+          Wand.expects(:execute_file_cmd).returns("image/jpeg\n")
+          assert_equal "image/jpeg", Wand.wave(FilePath.join(name).expand_path.to_s)
+        end
       end
     end
 
-    should "return nil when mime type and file fail" do
-      assert_nil Wand.wave('AVGARDD.eot')
-    end
+    context "when the file executable doesn't exist" do
+      setup do
+        Wand.executable = nil
+      end
 
-    should "detect executable" do
-      assert_equal '/usr/bin/file', Wand.executable
-    end
+      mime_types_gem_types.each_pair do |name, type|
+        should "use mime type gem if it returns type #{name}" do
+          assert_equal type, Wand.wave(FilePath.join(name).expand_path.to_s)
+        end
+      end
 
-    should "allow setting the executable" do
-      Wand.executable = '/usr/local/bin/file'
-      assert_equal '/usr/local/bin/file', Wand.executable
-    end
-
-    should "strip newlines and such" do
-      Wand.expects(:execute_file_cmd).returns("image/jpeg\n")
-      assert_equal "image/jpeg", Wand.wave(FilePath.join(name).expand_path.to_s)
+      unix_file_command_types.each_pair do |name, type|
+        should "returns nil when mime type returns nothing for #{name}" do
+          assert_nil Wand.wave(FilePath.join(name).expand_path.to_s)
+        end
+      end
     end
   end
 end
